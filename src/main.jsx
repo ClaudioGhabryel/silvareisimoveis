55838009b8d5799914d1d215fc96a2b65accb02f
ClaudioGhabryel <Claudioghabryel.cg@gmail.com>
2025-04-24 12:15:07 -0300

    Correção final para deploy no Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Cadastro from './pages/Cadastro';
-import Login from './pages/Login';
-import AreaCliente from './pages/AreaCliente';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/cadastro" element={<Cadastro />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/cliente" element={<AreaCliente />} />
-    </Routes>
-  </BrowserRouter>
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
